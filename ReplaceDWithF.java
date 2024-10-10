1e87da84728e2751cdf44a65e672d8f01fd8e48c
Dhiraj Desale <79238778+DhirajHDesale@users.noreply.github.com>
2024-10-10 12:25:33 +0530

    Replace D With F

---
+++ ReplaceDWithF.java
@@ -0,0 +1,22 @@
+
+public class MyClass 
+{
+	public static void main(String[] args) 
+	{
+		String s1 = "dddaa";
+		char ch1='d';
+		char ch2='f';
+		myCode(s1,ch1,ch2);
+	}
+
+	private static void myCode(String s1,char ch1,char ch2) 
+	{
+		char c[] = s1.toCharArray();
+		for(int i = 0;i<c.length;i++) 
+		{
+			if(c[i]==ch1) 
+			c[i] = ch2;
+		}
+		System.out.println(c);
+	}
+}
