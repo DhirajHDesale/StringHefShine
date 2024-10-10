8795cb5a7d4bc21604e66fcbbe10b32641555453
Dhiraj Desale <79238778+DhirajHDesale@users.noreply.github.com>
2024-10-10 13:02:24 +0530

    String Convert To Lower Case without in built method

---
+++ StringConvertToLowerCase.java
@@ -0,0 +1,22 @@
+
+public class MyClass 
+{
+
+	public static void main(String[] args) 
+	{
+		String s1 = "WELCOME";
+		myCode(s1);
+	}
+
+	private static void myCode(String s1) 
+	{
+		char c[]  = s1.toCharArray();
+		String ans = "";
+		for(int i = 0;i<c.length;i++) 
+		{
+			if(c[i]>='A' && c[i]<='Z')
+			ans = ans + (char)(c[i]+32);
+		}
+		System.out.println(ans);
+	}
+}
