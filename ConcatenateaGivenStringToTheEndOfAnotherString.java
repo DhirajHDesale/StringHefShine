bb565db92bdf73b3c8a0487bfba23fe6c1da80ae
DhirajHDesale <dhirajhanumantpatil@gmail.ocm>
2024-10-10 09:18:24 +0530

    Concatenate a Given StringTo The End Of Another String

---
+++ ConcatenateaGivenStringToTheEndOfAnotherString.java
@@ -0,0 +1,16 @@
+public class MyClass 
+{
+    
+	public static void main(String[] args) 
+	{
+		String s1 ="abcd";
+		String s2="efg";
+		myCode(s1,s2);
+	}
+	
+	private static void myCode(String s1,String s2) 
+	{
+		String ans = s1 + s2;
+		System.out.println(ans);
+	}
+}
