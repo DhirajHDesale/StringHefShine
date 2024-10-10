c9d96925be05f669e5901a44c5ba649fcfd4e402
Dhiraj Desale <79238778+DhirajHDesale@users.noreply.github.com>
2024-10-10 13:07:03 +0530

    remove Duplicate Element From String

---
+++ removeDuplicateEleFromString.java
@@ -0,0 +1,27 @@
+public class MyClass 
+{
+	
+	public static void main(String[] args) 
+	{
+		String s1 = "abbcfedafbcd";
+		myCode(s1);
+	}
+
+	private static void myCode(String s1) 
+	{
+		char c[] = s1.toCharArray();
+		String ans = "";
+		for(int i = 0;i<c.length;i++) 
+		{
+			int cnt = 0;
+			for(int j = 0;j<i;j++) 
+			{
+				if(c[i]==c[j])
+				cnt++;
+			}
+			if(cnt==0)
+			ans = ans + c[i];
+		}
+		System.out.println(ans);
+	}
+}
