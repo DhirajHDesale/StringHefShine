9ff01084c8937a094156dc57021b3184022053f0
Dhiraj Desale <79238778+DhirajHDesale@users.noreply.github.com>
2024-10-11 18:58:32 +0530

    Create String Array Sort By Length

---
+++ StringArraySortByLength.java
@@ -0,0 +1,30 @@
+public class MyClass 
+{
+
+	public static void main(String[] args) 
+	{
+		String s1 = "welcome to hef";
+		myCode(s1);
+	}
+	private static void myCode(String s1) 
+	{
+
+		String s[] = s1.split(" ");
+		for(int i = 0;i<s.length;i++) 
+		{
+			for(int j = i+1;j<s.length;j++) 
+			{
+				if(s[i].length()>s[j].length())
+				{
+					String t = s[i];
+					s[i] = s[j];
+					s[j] = t;
+				}
+			}
+		}
+		String ans = "";
+		for(String k:s)
+		ans = ans + k + " ";
+		System.out.println(ans);
+	}
+}
